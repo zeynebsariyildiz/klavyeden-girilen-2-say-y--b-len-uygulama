3f2a9477a09a3760f4ff4fba166c8f45345d17fe
Zeyneb Mehlika Sarıyıldız <sariyildiz.mehlika07@gmail.com>
2023-01-26 15:14:11 +0300

    hatalr düzeltildi

--- Program.cs
+++ Program.cs
@@ -10,18 +10,25 @@ namespace klavyeden_girilen_2_sayıyı_bölen_uygulama
     {
         static void Main(string[] args)
         {
-            int bolunen, bolen, bolum;
+            double bolunen, bolen, bolum;
 
             Console.WriteLine("Lütfen Bölünen Sayıyı Giriniz");
             bolunen = Int32.Parse(Console.ReadLine());
 
             Console.WriteLine("Lütfen Bölen Sayıyı Giriniz");
             bolen = Int32.Parse(Console.ReadLine());
-
-            bolum = bolunen / bolen;
-
-            Console.WriteLine("Çıkan Bölüm: {0}", bolum); //vayyy 
-            Console.ReadLine();
+            if (bolen==0 )
+            {
+                Console.WriteLine("Sonuç Belirsizdir");
+                Console.ReadLine();
+            }
+            else
+            {
+                bolum = bolunen / bolen;
+                Console.WriteLine("Çıkan Bölüm: {0}", bolum); //vayyy 
+                Console.ReadLine();
+            }
+            
 
             // bolen 0 olduğunda program hata veriyor
 
