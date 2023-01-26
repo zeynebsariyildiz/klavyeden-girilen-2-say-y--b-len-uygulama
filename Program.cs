6d5e5f97e1cc4c4c8f86928eddc13125c9bfd2a2
Zeyneb Mehlika Sarıyıldız <sariyildiz.mehlika07@gmail.com>
2023-01-26 10:45:54 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klavyeden_girilen_2_sayıyı_bölen_uygulama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bolunen, bolen, bolum;
+
+            Console.WriteLine("Lütfen Bölünen Sayıyı Giriniz");
+            bolunen = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Lütfen Bölen Sayıyı Giriniz");
+            bolen = Int32.Parse(Console.ReadLine());
+
+            bolum = bolunen / bolen;
+
+            Console.WriteLine("Çıkan Bölüm: {0}", bolum);
+            Console.ReadLine();
+
+        }
+    }
+}
